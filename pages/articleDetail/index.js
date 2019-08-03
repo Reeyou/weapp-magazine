55c5e13d12fc81cff065e40fdc1258843a9038c5
Reeyou <ryu993830137@163.com>
2019-08-03 14:03:48 +0800

    更新audio样式及进度条控制功能

--- pages/articleDetail/index.js
+++ pages/articleDetail/index.js
@@ -1,6 +1,8 @@
 // pages/articleDetail/index.js
-Page({
+var myAudio = wx.getBackgroundAudioManager()
 
+Page({
+  
   /**
    * 页面的初始数据
    */
@@ -23,14 +25,34 @@ Page({
         text: '第 3s 出现的弹幕',
         color: '#ff00ff',
         time: 3
-    }]
+    }],
+    isplay: false,
+    progressPercent: 0,
+    audioCurTime: 0,
+    progressWidth: 520,
+    progressCircleLeft: 0,
+    audioDuration: 400,
+    audioCircleStartX: 0,
+    audioCircleMoveX: 0,
+    audioProgressStartX: 0,
+    flag: false,
+    progressLeft: 0,
+    progressCircleWidth: 20
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getProgressLeft()
+  },
+  getProgressLeft: function() {
+    var that = this
+    wx.createIntersectionObserver().relativeToViewport().observe('#progress', (res) => {
+      this.setData({
+        progressLeft: (res.intersectionRect.left + this.data.progressCircleWidth / 2) * that.getPhoneRadio()
+      })
+    })
   },
   onVideoTap: function() {
     this.setData({
@@ -41,55 +63,123 @@ Page({
     myVideo.play()
   },
   onAudioTap: function() {
-    var myAudio = wx.getBackgroundAudioManager()
-    myAudio.src = ''
+    var isPlay = this.data.isPlay
+    if(!isPlay) {
+      this.onAudioPlay()
+    } else {
+      myAudio.pause()  
+    }
+    this.setData({
+      isPlay: !this.data.isPlay
+    })
   },
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  onAudioPlay: function() {
+    myAudio.title = '此时此刻'
+    myAudio.src = 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD&fromtag=46'
     
+    this.audioPlay()
+    this.audioUpdate()
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  audioPlay: function() {
+    var that = this
+    myAudio.onPause(function() {
+      that.setData({
+        isPlay: false
+      })
+    })
+    myAudio.onEnded(function() {
+      that.setData({
+        isPlay: false
+      })
+    })
+    myAudio.onStop(function() {
+      that.setData({
+        isPlay: false
+      })
+    })
+    myAudio.onPlay(function() {
+      that.setData({
+        isPlay: true
+      })
+    })
   },
+  audioUpdate: function() {
+    var that = this
+    myAudio.onTimeUpdate(function() {
+      var audioDuration = that.data.audioDuration
+      var audioCurTime = myAudio.currentTime.toFixed()
+      var progress = audioCurTime / audioDuration
+      var progressPercent = (audioCurTime / audioDuration) * 100
+      var progressCircleLeft = progress * that.data.progressWidth
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+      that.setData({
+        progressPercent: progressPercent,
+        audioCurTime: audioCurTime,
+        progressCircleLeft: progressCircleLeft
+      })
+    })
   },
+  onProgressStart: function(e) {
+    var audioProgressStartX = e.touches[0].pageX * this.getPhoneRadio()
+    if(audioProgressStartX > this.data.progressLeft) {
+      var progressWidth = this.data.progressWidth
+      var progressCircleLeft = audioProgressStartX - this.data.progressLeft
+      var progressPercent = progressCircleLeft / progressWidth
+      var audioCurTime = (this.data.audioDuration * progressPercent).toFixed()
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
+      this.onAudioPlay()
+      myAudio.seek(Number(audioCurTime))
 
+      this.setData({
+        progressCircleLeft: progressCircleLeft,
+        progressPercent: progressPercent * 100,
+        audioCurTime: audioCurTime
+      })
+    }
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
+  onAudioCircleStart: function(e) {
+    var audioCircleStartX = e.touches[0].pageX * this.getPhoneRadio()
+    
+    if(!this.data.flag) {
+      this.setData({
+        audioCircleStartX: audioCircleStartX,
+        flag: true
+      })
+    }
   },
+  onAudioCircleMove: function(e) {
+    var audioCircleStartX = this.data.audioCircleStartX 
+    var audioCircleMoveX = e.touches[0].pageX * this.getPhoneRadio()
+    var progressWidth = this.data.progressWidth
+    var progressCircleLeft = audioCircleMoveX - audioCircleStartX
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
+    if(progressCircleLeft <= 0) {
+      progressCircleLeft = 0
+    } else if(progressCircleLeft >= progressWidth) {
+      progressCircleLeft = progressWidth
+    }
 
+    var progressPercent = progressCircleLeft / progressWidth
+    var audioCurTime = (this.data.audioDuration * progressPercent).toFixed()
+
+    this.onAudioPlay()
+    myAudio.seek(Number(audioCurTime))
+    
+
+    this.setData({
+      progressCircleLeft: progressCircleLeft,
+      progressPercent: progressPercent * 100,
+      audioCurTime: audioCurTime
+    })
   },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  getPhoneRadio: function() {
+    var radio;
+    wx.getSystemInfo({
+      success: function(res) {
+        var width = res.screenWidth;
+        radio = 750 / width
+      }
+    })
+    return radio;
   }
 })
